2c7c052042ed46e23953cdd3e42a67d542890c49
TishaniWijenayake <IT22248930@my.sliit.lk>
2025-04-25 20:54:16 +0530

    initial backend setup

---
+++ backend/src/main/java/com/example/cookingsystem/CookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookingsystemApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/cookingsystem/CookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
