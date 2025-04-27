afeacb9395d0e29ad3cf47a21321068a86520c43
TishaniWijenayake <IT22248930@my.sliit.lk>
2025-04-27 21:29:45 +0530

    setup feedback repository

---
+++ backend/src/main/java/com/example/cookingsystem/repositories/FeedbackRepository.java
@@ -0,0 +1,12 @@
+package com.example.cookingsystem.repositories;
+
+import com.example.cookingsystem.models.Feedback;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FeedbackRepository extends MongoRepository<Feedback, String> {
+    List<Feedback> findAll();
+}
